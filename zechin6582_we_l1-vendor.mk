39c38e7955e15dc017da4be14879662454a1b6b5
BrunoEchevarria <bruno.ecq@gmail.com>
2017-05-27 15:41:12 -0700

    Upload for MM

---
+++ zechin6582_we_l1-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/alps/zechin6582_we_l1/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/alps/zechin6582_we_l1/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/alps/zechin6582_we_l1/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/alps/zechin6582_we_l1/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/alps/zechin6582_we_l1/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/alps/zechin6582_we_l1/priv-app,system/priv-app)
